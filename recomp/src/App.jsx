df4778e3136ec461a3ca189ef8a34f37e5988add
2025b01010374 <ritu.25b01010374@abes.ac.in>
2026-05-26 15:08:02 +0530

    Counter

---
+++ recomp/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Counter from './Counter.jsx';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
